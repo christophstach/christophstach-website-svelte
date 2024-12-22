7df85a1e10a59015a49d3370d65230734076c4ec
Christoph Stach <christoph.stach@gmail.com>
2024-12-22 13:30:25 +0100

    refactor: enhance project display layout and improve styling

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,10 +7,55 @@
 	<meta property="og:title" content="Christoph Stach - Projects" />
 </svelte:head>
 
-<div class="prose prose-indigo mx-auto sm:prose-sm lg:prose-lg xl:prose-xl dark:prose-invert">
-	Projects
+<div
+	class="max-w-screen-lg mx-auto prose prose-indigo sm:prose-sm lg:prose-lg xl:prose-xl dark:prose-invert"
+>
+	<h2>Projects</h2>
+</div>
 
+<div class="grid max-w-screen-lg grid-cols-3 gap-5 mx-auto mt-8">
 	{#each data.projects as project}
-		<pre>{JSON.stringify(project, null, 2)}</pre>
+		<div class="p-4 prose-sm prose border prose-indigo dark:prose-invert">
+			<h3>
+				{project.title}
+			</h3>
+			<div>
+				<div>{@html project.description}</div>
+				<div class="flex flex-wrap w-full gap-1 mt-5 overflow-hidden">
+					{#each project.tags as tag}
+						<div class="rounded bg-indigo-500 px-1 py-0.5 text-xs text-white">
+							{tag}
+						</div>
+					{/each}
+				</div>
+				<div class="mt-5">
+					{#if project.website}
+						<div class="flex gap-2">
+							<div>
+								<span class="iconify mt-1.5 size-4 tabler--link"></span>
+							</div>
+							<a href={project.website} target="_blank" rel="noopener noreferrer">
+								{project.website}
+							</a>
+						</div>
+					{/if}
+					{#if project.repository}
+						<div class="flex gap-2">
+							<div>
+								<span class="iconify mt-1.5 size-4 tabler--brand-github"></span>
+							</div>
+							<a
+								href={project.repository}
+								target="_blank"
+								class="overflow-hidden break-words"
+								rel="noopener noreferrer"
+							>
+								{project.repository}
+							</a>
+						</div>
+					{/if}
+				</div>
+			</div>
+		</div>
 	{/each}
 </div>
