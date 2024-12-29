420ca5b2937633b82daa70f32233941fb5ca0024
Christoph Stach <christoph.stach@gmail.com>
2024-12-29 10:59:56 +0100

    chore: remove prerender export from projects page server file

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,8 +1,6 @@
 import { db } from '../../db/client';
 import { projectsTable } from '../../db/schema/projects';
 
-export const prerender = false;
-
 export async function load() {
 	const projects = await db.select().from(projectsTable);
 
